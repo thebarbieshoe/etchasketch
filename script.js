e25e7f27d307079c58c819409429b3ca9632fb0b
Julie <mclaugja@gmail.com>
2017-07-03 21:56:44 -0400

    made loads of progress!

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
 $(document).ready(function(){
-	for (x=1; x<17;x++;) {
-var rows = 
-	$('#grid').append(magic);
-}
-	});
+	for (i=0; i<255; i++) {
+$(".grid").append('<div class="square">');
+};
+});
+
+$(document).ready(function(){
+$(".square").hover(function(){
+     $(this).addClass("squared");
+     }, function(){
+ }); 
+});
+
+
+$(document).ready(function(){
+$("button").on("click",function() {
+  $('div').removeClass("squared");
+  var gridRow = prompt("Select how many squares for each side of your new creation");
+  if (gridRow==64) {
+  		$(".square").remove();
+  		for (i=0; i<4096; i++) {
+  		$(".grid").append('<div class="squaredsix">');
+  	};
+  };
+});
+  });
+
+
+$(document).ready(function(){
+$(".squaresix").mouseenter(function(){
+     $(this).addClass("squaredsix");
+     }, function(){
+ }); 
+});
